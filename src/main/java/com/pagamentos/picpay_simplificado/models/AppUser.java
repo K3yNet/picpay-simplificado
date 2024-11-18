864f8555a4325f5be422f1d6881baa704ee26503
K3yNet <enzo_vel@hotmail.com>
2024-11-18 16:21:16 -0300

    Update AppUser.java

--- src/main/java/com/pagamentos/picpay_simplificado/models/AppUser.java
+++ src/main/java/com/pagamentos/picpay_simplificado/models/AppUser.java
@@ -2,8 +2,6 @@ package com.pagamentos.picpay_simplificado.models;
 
 import java.math.BigDecimal;
 
-import org.hibernate.validator.constraints.UniqueElements;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
